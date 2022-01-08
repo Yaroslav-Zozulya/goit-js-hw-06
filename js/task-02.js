7ed7acfb028adf4ee37882e122733fbfd31c8589
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-01-08 20:53:04 +0300

    complited tasks #1 #2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+function creatListItems(arr) {
+    const listEl = document.querySelector('#ingredients');
+    let listItemEl = '';
+    arr.forEach(element => {
+        listItemEl = document.createElement('li');
+        listItemEl.textContent = element;
+        listItemEl.classList.add('item');
+        listEl.append(listItemEl);
+    });
+}
+
+creatListItems(ingredients);
