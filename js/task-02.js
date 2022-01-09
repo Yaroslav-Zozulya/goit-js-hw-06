f94fc7bbb893204c792ff09f2d866b13bfb8af74
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-01-10 00:04:40 +0300

    reworked task #2

--- js/task-02.js
+++ js/task-02.js
@@ -7,15 +7,29 @@ const ingredients = [
     'Condiments',
 ];
 
-function creatListItems(arr) {
-    const listEl = document.querySelector('#ingredients');
-    let listItemEl = '';
-    arr.forEach(element => {
-        listItemEl = document.createElement('li');
-        listItemEl.textContent = element;
-        listItemEl.classList.add('item');
-        listEl.append(listItemEl);
-    });
-}
+const ingredientsListEl = document.querySelector('#ingredients ');
 
-creatListItems(ingredients);
+const ingredientsItemsEl = ingredients.map(ingredient => {
+    const ingredientItemEl = document.createElement('li');
+    ingredientItemEl.classList.add('item');
+    ingredientItemEl.textContent = ingredient;
+
+    return ingredientItemEl;
+});
+
+ingredientsListEl.append(...ingredientsItemsEl);
+
+//Была вот такая лабуда. Потом понял что я 100 дёргаю DOM :)
+// function creatListItems(arr) {
+//     const listEl = document.querySelector('#ingredients');
+//     const ingredientListEl = [];
+//     arr.forEach(element => {
+//         const listItemEl = document.createElement('li');
+//         listItemEl.textContent = element;
+//         listItemEl.classList.add('item');
+//         ingredientListEl.push(listItemEl);
+//     });
+//     listEl.append(...ingredientListEl);
+// }
+
+// creatListItems(ingredients);
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,61 @@ const images = [
         alt: 'Group of Horses Running',
     },
 ];
+
+const galleryEl = document.querySelector('.gallery');
+const galleryImagesEl = images.map(image => {
+    const galleryImageEl = document.createElement('img');
+    galleryImageEl.classList.add('galleryItem');
+    galleryImageEl.src = image.url;
+    galleryImageEl.alt = image.alt;
+    return galleryImageEl;
+});
+
+galleryEl.append(...galleryImagesEl);
+
+// const galleryEl = document.querySelector('.gallery');
+
+// const navItemEl = document.createElement('li');
+// navItemEl.classList.add('nav-item');
+
+// const navItemLinkEl = document.createElement('a');
+// navItemLinkEl.classList.add('nav-item__link');
+// navItemLinkEl.href = '#';
+// navItemLinkEl.textContent = 'четвёртый слайд';
+
+// navItemEl.appendChild(navItemLinkEl);
+
+// galleryEl.appendChild(navItemEl);
+// galleryEl.insertBefore(navItemEl, galleryEl.lastElementChild);
+
+// const colorPickerOptions = [
+//     { label: 'red', color: '#F44336' },
+//     { label: 'green', color: '#4CAF50' },
+//     { label: 'blue', color: '#2196F3' },
+//     { label: 'grey', color: '#607D8B' },
+// ];
+
+// const option = colorPickerOptions[3];
+
+// const buttonEl = document.createElement('button');
+// buttonEl.type = 'button';
+// buttonEl.textContent = option.label;
+// buttonEl.style.backgroundColor = option.color;
+// console.log(buttonEl);
+
+// document.body.append(buttonEl);
+
+// const galleryEl = document.querySelector('.gallery');
+
+// const slides = colorPickerOptions.map(slide => {
+//     const slideEl = document.createElement('button');
+//     slideEl.classList.add('slide');
+//     slideEl.type = 'button';
+//     slideEl.textContent = slide.label;
+//     slideEl.style.backgroundColor = slide.color;
+//     slideEl.style.width = '100px';
+//     slideEl.style.height = '50px';
+//     return slideEl;
+// });
+
+// galleryEl.append(...slides);
